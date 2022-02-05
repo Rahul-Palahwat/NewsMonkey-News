8c40c54a39277f4eff212561123ea737e22f885a
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-02-05 13:21:08 +0530

    Lect 39 changing class based to function based in newsmonkey

--- src/components/News.js
+++ src/components/News.js
@@ -1,55 +1,39 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
 
-export class News extends Component {
+const News = (props)=>{
 
-    static defaultProps={
-      country: 'in',
-      pageSize:8,
-      category:'general'
-    }
+  const[articles, setArticles] = useState([])
+  const[loading, setLoading] = useState(true)
+  const[page, setPage] = useState(1)
+  const[totalResults, setTotalResults] = useState(0)
+  // document.title=`NewsMonkey - ${this.capitalFirstLetter(props.category)}`;
+  
 
-    static propTypes={
-      country: PropTypes.string,
-      pageSize: PropTypes.number,
-      category: PropTypes.string
-    }
-
-    capitalFirstLetter=(string)=>{
+    const capitalFirstLetter=(string)=>{
       return string.charAt(0).toUpperCase()+string.slice(1);
     }
 
-    constructor(props){
-        super(props);
-        console.log("Hello I am a constructor from news component");
-        this.state={
-            articles: [],
-            loading: true,
-            page:1,
-            totalResults:0
-        }
-        document.title=`NewsMonkey - ${this.capitalFirstLetter(this.props.category)}`;
-    }
-
     // this is to remove al the extra lines of code from next prev and mount functions 
-    async updateNews(){
-        this.props.setProgress(10);
+    const updateNews=async ()=>{
+        props.setProgress(10);
       // console.log("componentDidMount");
-        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`;
-        this.setState({
-          loading:true
-        });
+        const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
+        
+        setLoading(true);
         let data=await fetch(url);
-        this.props.setProgress(30);
+        props.setProgress(30);
         let parsedData= await data.json();
-        this.props.setProgress(60);
+        props.setProgress(60);
         console.log(parsedData);
-        this.setState({articles: parsedData.articles , totalArticles:parsedData.totalResults, loading:false});
-        this.props.setProgress(100);
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
+        props.setProgress(100);
     }
 
 
@@ -57,49 +41,53 @@ export class News extends Component {
     // to fetch data from news api 
     // and yen render ka baad run hoga 
     // async and await yha pr promise return hone ka intazzar krenge 
-    async componentDidMount(){
-        this.updateNews();
-    }
+    // async componentDidMount(){
+    //     this.updateNews();
+    // }
+    // hooks ka use componentDidMount ki jgh 
+    useEffect(()=>{
+      updateNews();
+    }, [])
 
-    fetchMoreData = async () => {
-      this.setState({page: this.state.page+1});
-      const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+    const fetchMoreData = async () => {
+      setPage(page+1);
+      const url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
         
         let data=await fetch(url);
         let parsedData= await data.json();
         console.log(parsedData);
-        this.setState({articles: this.state.articles.concat(parsedData.articles) , totalArticles:parsedData.totalResults})
+        setArticles(articles.concat(parsedData.articles));
+        setTotalResults(parsedData.totalResults);
         
     };
 
 
     // // functions to handle clicks
     // handleNextClick = async ()=>{
-    // this.setState({page: this.state.page + 1});
+    // this.setState({page: page + 1});
     // this.updateNews();
 
     // } 
     // handlePrevClick=async()=>{
-    //   this.setState({page: this.state.page - 1});
+    //   this.setState({page: page - 1});
     //   this.updateNews();
     // }
 
-  render() {
     return (
       <>
-        <h1 className="text-center" style={{margin: '35px 0px'}}>NewMonkey - Top {this.capitalFirstLetter(this.props.category)} Headlines</h1>
-        {this.state.loading && <Spinner/>}
+        <h1 className="text-center" style={{margin: '35px 0px'}}>NewMonkey - Top {capitalFirstLetter(props.category)} Headlines</h1>
+        {loading && <Spinner/>}
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length!== this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length!== totalResults}
           loader={<Spinner/>}
         >
           <div className="container">
 
           
         <div className="row">
-        {this.state.articles.map((element)=>{
+        {articles.map((element)=>{
             // console.log(element);
             return <div className="col-md-4"  key={element.url}>
             <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
@@ -109,13 +97,25 @@ export class News extends Component {
         </div>
         </InfiniteScroll>
         {/* <div className="container d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-        <button disabled={this.state.page +1 >Math.ceil(this.state.totalArticles/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next&rarr;</button>
+        <button disabled={page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={page +1 >Math.ceil(totalArticles/props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next&rarr;</button>
         </div> */}
         
       </>
     );
-  }
+  
+}
+
+News.defaultProps={
+  country: 'in',
+  pageSize:8,
+  category:'general'
+}
+
+News.propTypes={
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string
 }
 
 export default News;
